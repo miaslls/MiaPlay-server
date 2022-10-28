b5d6d4c8b16eee569a5b3e2c441a74cf89d4369f
miaslls <97785569+miaslls@users.noreply.github.com>
2022-10-28 04:02:22 -0300

    fix

--- src/games/games.service.js
+++ src/games/games.service.js
@@ -1,12 +1,12 @@
 import Game from './Game.js';
 
-export const getAll = () => Game.find().populate('categories');
+export const getAll = () => Game.find().populate('genres');
 
-export const add = (body) => Game.create(body);
+export const add = (body) => Game.create(body).populate('genres');
 
-export const findById = (id) => Game.findById(id).populate('categories');
+export const findById = (id) => Game.findById(id).populate('genres');
 
 export const update = (id, body) =>
-  Game.findByIdAndUpdate(id, body).setOptions({ returnOriginal: false }).populate('categories');
+  Game.findByIdAndUpdate(id, body).setOptions({ returnOriginal: false }).populate('genres');
 
 export const remove = (id) => Game.findByIdAndDelete(id);
