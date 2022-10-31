922a390e32ea61206c437cd2a9f33321c06fb974
miaslls <97785569+miaslls@users.noreply.github.com>
2022-10-30 23:11:30 -0300

    unique genre for GenreGameList

--- src/genreGameLists/GenreGameList.js
+++ src/genreGameLists/GenreGameList.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const GenreGameListSchema = new mongoose.Schema(
   {
-    genre: { type: mongoose.Schema.Types.ObjectId, ref: 'Genre' },
+    genre: { type: mongoose.Schema.Types.ObjectId, ref: 'Genre', unique: true },
     games: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Game' }],
   },
   { versionKey: false },
