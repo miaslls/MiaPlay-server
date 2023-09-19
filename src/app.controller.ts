ff4170e51ec986d19b373d61540d1d86e6a275fd
miaslls <97785569+miaslls@users.noreply.github.com>
2023-09-19 01:28:51 -0300

    install and setup swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 import { AppService } from './app.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('status')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'get server status' })
   getAppStatus(@Req() req: Request) {
     const baseUrl = req.protocol + '://' + req.get('host');
     return this.appService.getAppStatus(baseUrl);
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('miaplay - server')
+    .setDescription('')
+    .setVersion('2.0.0')
+    .addTag('status')
+    .addTag('genres')
+    .addTag('games')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api-docs', app, swaggerDocument);
 
   app.enableCors({
     origin: ['https://miaplay.vercel.app/', 'http://localhost:3000/'],
