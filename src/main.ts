a689870ecb16c76085fadcca74afd1f10f033163
miaslls <97785569+miaslls@users.noreply.github.com>
2023-09-19 01:08:41 -0300

    setup cors + enableShutdownHooks

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: ['https://miaplay.vercel.app/', 'http://localhost:3000/'],
+  });
+
+  app.enableShutdownHooks();
+
+  await app.listen(port);
 }
 bootstrap();
